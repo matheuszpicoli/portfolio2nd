430ddbe879671c18086fba19dd7000e3675e40cd
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-09-27 21:59:57 -0300

    home page #1

---
+++ src/app/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+export default function Home(): React.JSX.Element {
+    return (
+        <section className="home">
+            <div className="image"></div>
+            <div className="main-content">
+                <strong>Transforme as suas aplicações deixando elas</strong>
+            </div>
+        </section>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "@/app/components/Navbar"
+import Home from "@/app/components/Home"
 
 export default function Portfolio(): React.JSX.Element {
     return (
         <React.Fragment>
             <Navbar />
+            <Home />
         </React.Fragment>
     )
 }
