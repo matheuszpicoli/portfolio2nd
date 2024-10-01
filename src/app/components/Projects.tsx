3dd8c3670212d809f86f4472ec315bc13474cabc
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-10-01 20:26:01 -0300

    making a slider with swiper for projects

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,31 +1,44 @@
+"use client"
+
 import React from "react"
+
 import * as Icon from "@/app/assets/icons"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination } from "swiper/modules"
+import "swiper/scss"
+import "swiper/scss/navigation"
+import "swiper/scss/pagination"
 
 export default function Projects(): React.JSX.Element {
     return (
         <section className="projects">
             <h2>Projetos</h2>
             <article className="viewer">
-                <div className="project">
-                    <video className="video" autoPlay muted loop>
-                        <source src={"videos/spaceship.mp4"} type="video/mp4" />
-                    </video>
-                </div>
-                <div className="description">
-                    <h3>MP Spaceship</h3>
-                    <p>MP SpaceShip é um jogo de nave onde você controla uma espaçonave azul para destruir inimigos brancos. Oferece três níveis de dificuldade e é totalmente executado em um arquivo HTML, manipulando a DOM diretamente. Utiliza Gulp para automação, SCSS para estilização, SweetAlert para modais e Particles JS para efeitos visuais. Feito em JavaScript puro.</p>
-                    <div className="technologies">
-                        <Icon.Css aria-label="css" />
-                        <Icon.Gulp aria-label="gulp" />
-                        <Icon.Html aria-label="html" />
-                        <Icon.JavaScript aria-label="javascript" />
-                        <Icon.Scss aria-label="scss" />
-                    </div>
-                    <div className="actions">
-                        <button type="button"><Icon.Eye />Deploy</button>
-                        <button type="button"><Icon.Github />Repositório</button>
-                    </div>
-                </div>
+                <Swiper modules={[Navigation, Pagination]} navigation pagination loop slidesPerView={1}>
+                    <SwiperSlide>
+                        <div className="project">
+                            <video className="video" autoPlay muted loop>
+                                <source src={"videos/spaceship.mp4"} type="video/mp4" />
+                            </video>
+                        </div>
+                        <div className="description">
+                            <h3>MP Spaceship</h3>
+                            <p>MP SpaceShip é um jogo de nave onde você controla uma espaçonave azul para destruir inimigos brancos. Oferece três níveis de dificuldade e é totalmente executado em um arquivo HTML, manipulando a DOM diretamente. Utiliza Gulp para automação, SCSS para estilização, SweetAlert para modais e Particles JS para efeitos visuais. Feito em JavaScript puro.</p>
+                            <div className="technologies">
+                                <Icon.Css aria-label="css" />
+                                <Icon.Gulp aria-label="gulp" />
+                                <Icon.Html aria-label="html" />
+                                <Icon.JavaScript aria-label="javascript" />
+                                <Icon.Scss aria-label="scss" />
+                            </div>
+                            <div className="actions">
+                                <button type="button"><Icon.Eye />Deploy</button>
+                                <button type="button"><Icon.Github />Repositório</button>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
             </article>
         </section>
     )
--- src/app/equipment/page.tsx
+++ src/app/equipment/page.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { default as Deskpad } from "@/app/assets/deskpad.png"
 import { default as Headset } from "@/app/assets/headset.png"
 import { default as Keyboard } from "@/app/assets/keyboard.png"
+import { default as Laptop } from "@/app/assets/laptop.png"
 import { default as Mouse } from "@/app/assets/mouse.png"
 import { default as Projector } from "@/app/assets/projector.png"
 
@@ -15,8 +16,9 @@ export default function Equipment(): React.JSX.Element {
             <p>Nessa seção coloquei os equipamentos que eu utilizo, aprovo cada item aqui citado.</p>
             <article className="cards">
                 <Card image={Deskpad} content="Deskpad RGB Exbom" category="Periférico" />
-                <Card image={Headset} content="Headset Multilaser Warrior Rama" category="Som" />
+                <Card image={Headset} content="Headset Multilaser Warrior Rama" category="Áudio" />
                 <Card image={Keyboard} content="Machenike K500 B94" category="Periférico" />
+                <Card image={Laptop} content="Dell Inspiron 13 5330 i7 1360P" category="Principal" />
                 <Card image={Mouse} content="Redragon Griffin" category="Periférico" />
                 <Card image={Projector} content="Projetor Astronauta" category="Iluminação" />
             </article>
